f38dddab8129fba662f280ca2f2042daedf13b46
Michael Lee <mike.epiphany@gmail.com>
2016-04-11 18:25:46 -0700

    add remember_me function to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = @user.auth_token
+      else
+        cookies[:auth_token] = @user.auth_token
+      end
+      # session[:user_id] = @user.id
       redirect_to root_path, notice: "Logged in!"
     else
       flash[:alert] = "Wrong email or password!"
@@ -14,7 +19,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    cookies.delete(:auth_token)
+    # session[:user_id] = nil
     redirect_to root_path, notice: "Logged out!"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 class User < ActiveRecord::Base
   has_secure_password
 
@@ -21,6 +22,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, if: :changing_password?
 
   validates :password_confirmation, presence: true, if: :changing_password?
+
+  before_create { generate_token(:auth_token)}
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 
   def full_name
     "#{first_name} #{last_name}".titleize
