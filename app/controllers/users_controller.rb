a47438336d2eb259f12e3e47c7f668dd228b935c
Michael Lee <mike.epiphany@gmail.com>
2016-04-14 18:34:25 -0700

    add strong params to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    user_params = params.require(:user).permit(:first_name, :last_name, :email,
+                                               :password, :password_confirmation,
+                                               :auth_token, :password_reset_token,
+                                               :password_reset_sent_at   )
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path, notice: "Account has been created!"
